67519f4cfa0641cf286f7297e391ea551399f286
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-04 22:24:30 +0600

    [Done] Error handler added

--- src/components/PlayersReactQueries.js
+++ src/components/PlayersReactQueries.js
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { useQuery } from "react-query";
 
-const fetchdata = () => {
-  return axios.get("http://localhost:4000/players");
+const fetchdata = async () => {
+  return await axios.get("http://localhost:4000/players");
 };
 const PlayersReactQueries = () => {
-  const { isloding, data } = useQuery("players", fetchdata);
+  const { isloding, data, isError, error } = useQuery("players", fetchdata);
 
   //Loading If data fetch takes time
   if (isloding) {
     return <h2>Loading...</h2>;
+  }
+
+  // handle Error
+  if (isError) {
+    return <h2> {error?.message}</h2>;
   }
 
   return (
