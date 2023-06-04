904242fadcf6616d0435a119b69130d1c624c096
Mrinmoy Kumer Ray <baust.cse.170201073@gmail.com>
2023-06-04 22:56:13 +0600

    [Done] ReactQueryDebtools Added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Router/Router";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router}></RouterProvider>
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   );
 }
